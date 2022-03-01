af202218fcdbe6b85dbd7b968c2d7753d7d7b17f
Kuba <plajerowy@gmail.com>
2022-03-01 22:23:32 +0100

    Update scripts.js
    Vue.js update - add to basket

--- app/spacex/static/scripts.js
+++ app/spacex/static/scripts.js
@@ -1,64 +1,36 @@
-let contact = Vue.createApp({
-
-    data(){
-        return{
-
-        }
-    }
-})
-contact.component('contact-form',{
+let app = Vue.createApp({
     delimiters: [ '[[', ']]' ],
-    template: `
-        <form @submit.prevent="save()">
-        <h3>[[ title ]]</h3>
-            <div class="form-group">
-                <input type="text" v-model="username" required class="form-control" id="username" aria-describedby="username" placeholder="Full Name">
-            </div>
-            <div class="form-group">
-                <input type="email" v-model="email" required class="form-control" id="mail" placeholder="Email">
-            </div>
-            <div class="form-check">
-
-                <textarea name="id_message" v-model="message" required class="form-control" placeholder="Your message" id="id_message" cols="80" rows="5"></textarea>
-                <br>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </div>
-
-
-
-
-
-        </form>
-    `,
-    data(){
-        return{
-            title: 'Contact form',
-            username: 'boyeo',
-            email: 'plajerowy@gmail.com',
-            message: 'message'
-        }
-    },
     methods:{
-
-        save(){
-            let pack = {
-                username: this.username,
-                email: this.email,
-                message: this.message
-            }
-            fetch('/contact/', {
-                credentials: 'include',
-                method: 'POST',
-                meta:{requiresAuth: false},
-                headers: {'Content-Type': 'application/json; charset=UTF-8', 'Accept': 'application/json',},
-                body: JSON.stringify(pack)
-            })
-                .then(res => console.log(res))
-                .then((json) => console.log(json ))
-
+      deleteItem: function(id){
+        if(confirm('Are you sure to remove this item?')){
+          let data = {
+            method: 'POST'
+          };
+          fetch('/delete/'+id +'/', data)
+          .then(response => response.json())
         }
-
+      },
+      addToBasket: function(id){
+        console.log(id);
+        let data = {
+            method: "POST",
+            credentials: 'include',
+            body: id,
+            id: id
+        }
+        fetch('/add_to_basket/' + id, data)
+        .then(response => response.json())
+      },
+      show_basket: function(){
+        console.log('hover')
+      }
     }
 })
 
-contact.mount("#contact")
+app.component('slide-basket', {
+  template: `
+
+  `
+});
+
+app.mount("#app")
